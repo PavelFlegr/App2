9524c3f8644f603f14566b9b79faf87ced36479c
flegrpa14 <dflegrpa14@DST40010>
2017-05-29 11:15:27 +0200

    moved alarm logic to pcl; hopefully stopped app from not responding

--- App2/App2.Android/AlarmReciever.cs
+++ App2/App2.Android/AlarmReciever.cs
@@ -17,65 +17,20 @@ namespace App2.Android
     [BroadcastReceiver]
     public class AlarmReciever : BroadcastReceiver
     {
-        Context context;
-
         public override async void OnReceive(Context context, Intent intent)
         {
             //this is neccessary because it's the only way to avoid deadlock here
             var status = GoAsync();
 
-            this.context = context;
-            await CheckNearbyLocations();
-            
-            //notify android we are done; otherwise the app will be flagged as not responding
-            status.Finish();
-
-        }
-
-        async Task<Plugin.Geolocator.Abstractions.Position> GetPosition()
-        {
-            var locator = CrossGeolocator.Current;
-            return await locator.GetPositionAsync();
-        }
-
-        async Task CheckNearbyLocations()
-        {
-            var pos = await GetPosition();
-            var position = new Position(pos.Latitude, pos.Longitude);
-            var locations = LocationDB.GetLocationList();
-            foreach (Location location in locations)
+            try
             {
-                double distance = DistanceCalculator.Distance(location.Coords, position);
-                if (location.Timeout && distance > location.Radius + 10)
-                {
-                    location.Timeout = false;
-                }
-                if(location.Active && !location.Timeout && distance < location.Radius)
-                {
-                    NotifyNearby(location);
-                    location.Timeout = true;
-                }
-
-                LocationDB.SaveItem(location);
+                await AlarmHandler.OnAlarm();
             }
-            
-        }
-
-        double GetDistance(Position pos1, Position pos2)
-        {
-            return Math.Sqrt(Math.Pow(pos2.Latitude - pos1.Latitude, 2) + Math.Pow(pos2.Longitude - pos1.Longitude, 2));
-        }
-
-        void NotifyNearby(Location location)
-        {
-            Notification.Builder b = new Notification.Builder(context)
-                .SetSmallIcon(Resource.Drawable.notification_template_icon_bg)
-                .SetContentTitle(location.Title)
-                .SetContentText(location.Description)
-                .SetVibrate(new long[] { 0, 500 });
-                
-            NotificationManager mNotificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
-            mNotificationManager.Notify(0, b.Build());
+            finally
+            {
+                //notify android we are done; otherwise the app will be flagged as not responding
+                status.Finish();
+            }
         }
     }
 }
